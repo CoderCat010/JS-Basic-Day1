23b9ffcc3872cb2b1b2baf19494ff6dec96ad8a3
CoderCat01 <nusraumme@gmail.com>
2024-07-31 16:47:00 +0600

    added notes

---
+++ practice3.js
@@ -0,0 +1,57 @@
+// SIMPLE MATH OPERATIONS
+
+var add1 = 5;
+var add2 = 10;
+var result = add1 + add2;
+
+// console.log(result);
+
+// another way 
+var add = 5 + 10;
+console.log(add);
+
+// another way 
+// console.log(5 + 10);
+
+
+// SHORTHAND MATH OPERATIONS
+/* 
+* 1. addition +=
+* 2.  Subtraction -+
+* 3. multiplication *=
+* 4. divide /=
+* 6. modulus %=
+* 7. increment ++
+* 8. decrement --
+ */
+
+EXAMPLES
+var add1 = 5;
+var add2 = 10;
+add1 +=  add2;
+
+// console.log(add1);
+
+var add1 = 5;
+var add2 = 10;
+add1 -=  add2;
+
+// console.log(add1);
+
+var add1 = 5;
+var add2 = 10;
+add1 *=  add2;
+
+// console.log(add1);
+
+var add1 = 5;
+var add2 = 10;
+add1 /=  add2;
+
+// console.log(add1);
+
+var add1 = 5;
+var add2 = 10;
+add1 %=  add2;
+
+// console.log(add1);
