81ee2f8bea0b3e6e6f725364d1eb8dd6bf66c61c
CoderCat01 <nusraumme@gmail.com>
2024-07-31 17:03:06 +0600

    added my 5th practice tasks

---
+++ practice6.js
@@ -0,0 +1,119 @@
+//!========SIMPLE COMPARSION IN VARIABLES
+
+var num1 = 5;
+var num2 = 6;
+var compare = 5 == 6;
+
+// console.log(compare);
+
+var num1 = 10;
+var num2 = 20;
+
+// console.log(num1 != num2);
+
+let string1 = 'tara';
+let string2 = 'tutu';
+let comparestrings = string1 == string2;
+
+// console.log(comparestrings);
+
+var string = 'tara';
+var number = 30;
+
+// console.log(string != number);
+
+
+//!=========JS CONDITIONALS STATEMENT, IF-ELSE, COMPARISION
+
+/* *JS LOGICAL OPERATORS
+ * 1. && And Operator
+ * 2. || Or Operator
+ * 3. ! Not Operator
+ * */
+
+//?simple comparison using by if-else 
+
+var myMoney = 300;
+var chickenPrice = 450;
+
+// if(myMoney > chickenPrice){
+//     console.log('kutush kore chibai chibai chicken khamu!!!');
+// } else{
+//     console.log('taka nai gorbis manush');
+// }
+
+
+
+//?comparison using by && and if-else 
+var myMoney = 500;
+var coldweather = false;
+var chickenPrice = 450;
+
+// if(myMoney > chickenPrice && coldweather == true){
+//     console.log('kutush kore chibai chibai chicken khamu!!!');
+// } else{
+//     console.log('thak khamu na vajapora khaite nai...');
+// }
+
+var sallery = 50000;
+var ovodrochele = false;
+var car = 2;
+
+// if(sallery == 50000 && ovodrochele == false && car >= 2){
+//     console.log('esho botsho biye kore feli!!!');
+// }else{
+//     console.log('tomar kopale biye nei botsho very very ssad...');
+// }
+
+var mysallery = 50000;
+var car = 2;
+var sundori = false;
+
+// if (mysallery == 50000 && car >= 2) {
+//     if (handsome == true) {
+//         console.log('cholo cokinaaa prem kori!');
+//     } else {
+//         console.log('cholo cokinaaa prem kori! tumi sundori na tao cholbe karon tekaaaaaaa ache to!!!');
+//     }
+// } else {
+//     console.log('okormer dheki...prem korar skill taw nai shi shi sih!');
+// }
+
+
+
+//?comparison using by || and if-else 
+var mysallery = 50000;
+var car = 1;
+
+// if(mysallery == 60000 || car >= 2){
+//     console.log('esho botsho biye kore feli!!!');
+//     }else{
+//         console.log('tomar kopale biye nai botsho very very ssad...');
+// }
+
+
+//?comparison using by both &&,|| and if-else
+var isGradutaed = true;
+var isSingle = true;
+var issallery = 40000;
+var car = 2;
+
+// if(isGradutaed == true && isSingle == true && (sallery >= 70000 ||  car >= 2)){
+//     console.log('cholo biye kore feli')
+// }else{
+//     console.log('tor kopale biya nai bujjos ')
+// }
+
+var physicsMark = 99;
+var englishMark = 7;
+var mathMark = 8;
+
+// if(physicsMark >= 80 && (englishMark > 60 || mathMark > 70)){
+//     console.log('woww congratulations!!');
+// }else{
+//     console.log('try again...you can do better next time!');
+// }
+
+
+
+
