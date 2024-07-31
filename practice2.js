d516114f567c984c70495e72c0f2649ca8ed3d80
CoderCat01 <nusraumme@gmail.com>
2024-07-31 16:45:43 +0600

    added my 2nd practice tasks

---
+++ practice2.js
@@ -0,0 +1,18 @@
+// variable common naming convention
+
+// camelCase
+var myName = 'jani na';
+
+// snake_case
+var my_name = 'jani naaa';
+
+// PascaleCase
+var MyName = 'jani naaaaaaaa';
+
+// also we can use
+/*
+* 1. var $myname = dollar sign
+* 2. var myname  = all lowercase
+* 3. var MYNAME = all uppercase
+* 4. var Myname = upperlowercase
+*/
