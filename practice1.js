16fa45d0fb272932fbb2d57885b7ff23238ee1dc
CoderCat01 <nusraumme@gmail.com>
2024-07-31 16:42:53 +0600

    added my first js practices

---
+++ practice1.js
@@ -0,0 +1,39 @@
+//!Mathematical Operation Shorthand
+//Number: 1 Operation Shorthand
+
+// //*amra jevabe jog kori
+// var price1 = 50;
+// price1 = 50 + 40; 
+// console.log(price1)
+
+//  //* shortcut e jeabe jog korte hoy
+// var price1 = 50;
+// price1 += 40; //price1 er current value mane 50 er sathe notun kore 40 add kora hoice 
+// console.log(price1)
+
+// //* shortcut e jeabe biog korte hoy
+// var price1 = 50;
+// price1 -= 40;  
+// console.log(price1)
+
+// //* shortcut e jeabe gun korte hoy
+// var price1 = 50;
+// price1 *= 40; 
+// console.log(price1)
+
+// //* shortcut e jeabe vag korte hoy
+// var price1 = 50;
+// price1 /= 40; 
+// console.log(price1)
+
+//  alada aldda variable declare kore jog biog gun vag korar khetre shorthand kaj kore na.
+
+//Number: 2 Operation Shorthand
+
+// var love = 100;
+// love++; //love er current value mane 100 er sathe notun kore 1 add hoice/ ak barce
+// console.log(love);
+
+// var love = 100;
+// love--; //love er current value mane 100 er sathe notun kore 1 biog hoice/ ak komce
+// console.log(love);
