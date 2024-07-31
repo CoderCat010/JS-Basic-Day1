d65060a7347fd5adf690fd825d694c02720b7567
CoderCat01 <nusraumme@gmail.com>
2024-07-31 17:47:33 +0600

    added my 6th practice tasks

---
+++ practice7.js
@@ -0,0 +1,39 @@
+// // while loop
+//  //odd numbers
+// var oddNumbers = 1;
+// while(oddNumbers < 10){
+//     console.log(oddNumbers);
+//     oddNumbers += 2;
+// }
+ //even numbers
+/* var oddNumbers = 1;
+while(oddNumbers < 10){
+    console.log(oddNumbers);
+    oddNumbers += 2;
+} */
+
+
+// var displayNumbers = 0; 
+// while(displayNumbers <= 10){
+//    console.log(displayNumbers)
+//    displayNumbers += 2;
+// } // ++ দিয়ে এক এক বাড়ছে এবং ++ = 2 দেওয়াতে 2 করে বাড়ছে।
+
+
+// display 10 to 0 using by for loop
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i);
+// }
+
+
+// // post increment
+// var x = 5;
+// console.log(x++); //output 5  
+// console.log(x++); // ourput 6
+
+// // pre increment 
+// var y = 10;
+// console.log(++y); // output 11
+// console.log(++y); // output 12
+
+
